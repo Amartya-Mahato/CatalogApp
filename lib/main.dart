291ebdc0786c18cc75ca23b279ee3eea3133a353
Amartya Mahato <amartyaipsita135@gmail.com>
2022-05-23 14:42:03 +0530

    day1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Material(
+            child: Center(
+                child: Container(
+                    child: Text(
+      "Hello Fuckers",
+      style: TextStyle(color: Colors.pink),
+    )))));
+  }
+}
