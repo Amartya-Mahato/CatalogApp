26cc18d97937096afe59e14ea42feee7a541b4ca
Amartya Mahato <amartyaipsita135@gmail.com>
2022-05-23 15:58:57 +0530

    day2

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Home_pages.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Home_pages(key),
