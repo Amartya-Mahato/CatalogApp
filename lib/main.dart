bfefe10be227286a753003694df843196265116a
Amartya Mahato <amartyaipsita135@gmail.com>
2022-05-25 01:34:56 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/Home_pages.dart';
 import 'package:flutter_application_1/pages/Page_two.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const App());
@@ -8,24 +9,25 @@ void main() {
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
-  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: Home_pages(key),
+      initialRoute: "/login",
       routes: {
-        // "/": (context) => Home_pages(key),
-        "/": (context) => Page_two(),
+        "/": (context) => Home_pages(key),
         "/login": (context) => Page_two(),
       },
+
       themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primarySwatch: Colors.deepPurple,
       ),
       darkTheme: ThemeData(
-        // brightness: Brightness.dark,
-        primarySwatch: Colors.deepPurple,
+        brightness: Brightness.dark,
+        // primarySwatch: Colors.red,
       ),
     );
   }
--- lib/pages/Page_two.dart
+++ lib/pages/Page_two.dart
@@ -1,22 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Page_two extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Hi how are you",
-          style: TextStyle(
-            // background: Colors.red,
-            fontSize: 10,
-            color: Colors.yellow,
-            fontWeight: FontWeight.w400,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/image/undraw_Login_re_4vu2.png",
+            fit: BoxFit.contain,
           ),
-          textScaleFactor: 5.0,
-        ),
+          SizedBox(
+            height: 3,
+          ),
+          Text(
+            "Welcome",
+            style: GoogleFonts.lato(
+              textStyle: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            textScaleFactor: 2.0,
+          ),
+          // SizedBox(
+          //   height: 5,
+          // ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 0.0, horizontal: 30.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                    hintText: "Enter username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Enter password",
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          ElevatedButton(
+              child: Text(
+                "Login",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w200),
+              ),
+              style: TextButton.styleFrom(),
+              onPressed: () {
+                // print("game");
+              }),
+        ],
       ),
-      color: Colors.deepPurple,
     );
   }
 }
