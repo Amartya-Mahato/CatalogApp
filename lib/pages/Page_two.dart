5e09058a7414c06368eedab160323172189e7ddd
Amartya Mahato <amartyaipsita135@gmail.com>
2022-05-27 19:13:51 +0530

    day6

--- lib/pages/Page_two.dart
+++ lib/pages/Page_two.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Page_two extends StatelessWidget {
+class Page_two extends StatefulWidget {
+  @override
+  State<Page_two> createState() => _Page_twoState();
+}
+
+class _Page_twoState extends State<Page_two> {
+  
+  String str = "";
+  bool chkButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +27,7 @@ class Page_two extends StatelessWidget {
               height: 3,
             ),
             Text(
-              "Welcome",
+              "Welcome $str",
               style: GoogleFonts.lato(
                 textStyle: TextStyle(
                   fontSize: 10,
@@ -38,6 +47,11 @@ class Page_two extends StatelessWidget {
                       labelText: "Username",
                       hintText: "Enter username",
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        str = value;
+                      });
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -52,15 +66,46 @@ class Page_two extends StatelessWidget {
             SizedBox(
               height: 30.0,
             ),
-            ElevatedButton(
-                style: TextButton.styleFrom(minimumSize: Size(160, 40)),
-                child: Text(
-                  "Login",
-                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w200),
+
+            InkWell(
+              onTap: () async {
+                setState(() {
+                  chkButton = true;
+                });
+                await Future.delayed(Duration(milliseconds: 900));
+                Navigator.pushNamed(context, Routes.homeRoute);
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                height: 40,
+                width: chkButton ? 40 : 150,
+                // color: Colors.deepPurple,
+                alignment: Alignment.center,
+                child: chkButton
+                    ? Icon(
+                        Icons.done,
+                        color: Colors.white,
+                      )
+                    : Text("Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        )),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(chkButton ? 50 : 8),
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, Routes.homeRoute);
-                }),
+              ),
+            ),
+            // ElevatedButton(
+            //     style: TextButton.styleFrom(minimumSize: Size(160, 40)),
+            //     child: Text(
+            //       "Login",
+            //       style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w200),
+            //     ),
+            //     onPressed: () {
+            //       Navigator.pushNamed(context, Routes.homeRoute);
+            //     }),
           ],
         ),
       ),
