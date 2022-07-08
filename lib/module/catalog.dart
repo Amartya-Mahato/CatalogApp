f1af764758994f84ca2537ac9aa3ca43a4faa9c4
Amartya Mahato <amartyaipsita135@gmail.com>
2022-07-08 17:40:35 +0530

    day16

--- lib/module/catalog.dart
+++ lib/module/catalog.dart
@@ -1,7 +1,12 @@
 import 'dart:convert';
 
 class CatalogModel {
-  static  List<Item> item = [];
+  static List<Item> item = [];
+
+  static Item getById(int id) =>
+      item.firstWhere((element) => element.id == id, orElse: null);
+
+  static Item getByPosition(int pos) => item[pos];
 }
 
 class Item {
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widget/Theme/theme.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        color: context.canvasColor,
+      ),
+    );
   }
 }
