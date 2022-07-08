9cbed85a1f969d92292806b811b8ed5a4c4f7a76
Amartya Mahato <amartyaipsita135@gmail.com>
2022-07-09 04:21:42 +0530

    day17

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -11,9 +11,65 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Container(
-        color: context.canvasColor,
+      body: Column(
+        children: [
+          _CartList().pOnly(left: 16, right: 16).expand(),
+          Divider(
+            color: context.backgroundColor,
+            thickness: 3,
+          ),
+          _CardTotal(),
+        ],
       ),
     );
+  }
+}
+
+class _CardTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 80,
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          "\$9999".text.bold.xl2.color(Colors.pink).make(),
+          Spacer(),
+          ElevatedButton(
+            onPressed: () => print("cart"),
+            child: "Buy".text.bold.color(Colors.white).make(),
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(context.backgroundColor),
+            ),
+          ).w32(context),
+        ]).p12());
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  _CartListState createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 30,
+        itemBuilder: (context, index) => ListTile(
+              leading: Icon(
+                Icons.done,
+                color: Colors.green,
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.remove_circle_outline),
+                onPressed: () {},
+              ),
+              title: "Item 1"
+                  .text
+                  .color(context.isDarkMode
+                      ? MyTheme.creamColor
+                      : MyTheme.darkBluishColor)
+                  .make(),
+            ));
   }
 }
